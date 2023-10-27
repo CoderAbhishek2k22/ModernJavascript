fca1b85a528a68d8acbe14d283007915691eef86
CoderAbhishek2k22 <121576319+CoderAbhishek2k22@users.noreply.github.com>
2023-10-27 18:38:46 +0000

    configuration

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 23456;
+let accountEmail = 'abhishekmandal123@gmail.com';
+var accountPassword = '123456789';
+accountCity = 'Faridabad';
+let accountState;
+
+
+// accountId = 2; // Not Allowed;
+
+accountEmail = 'testing123@gmail.com';
+accountPassword = '987654321';
+accountCity = 'Palwal';
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
