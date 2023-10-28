37de5daac6c8f9ad5649fad500e526f01f1003ff
CoderAbhishek2k22 <121576319+CoderAbhishek2k22@users.noreply.github.com>
2023-10-28 07:43:55 +0000

    data types conversion

---
+++ 01_Basic/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = '33ab';
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueNumber = Number(score);
+// console.log(typeof(valueNumber));
+// console.log(valueNumber);
+
+let isLoggedIn = 1;
+let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof(booleanLoggedIn));
+// console.log(booleanLoggedIn);
+
+// 0 => false , '' => false
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof(stringNumber));
+console.log(stringNumber);
